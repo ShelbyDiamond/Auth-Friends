ac1cde1d4cd8e6a744720154ca90d22c7056f5b4
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-20 16:40:32 -0700

    Updated files to render login.
    updated imports to fix bugs

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,14 +1,14 @@
 import React from "react"
-import Login from "./components/Login"
+import Login from "./Components/Login"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import PrivateRoute from "./components/PrivateRoute"
+// import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
     <Router>
       <div>
         <Link to="/login">Login</Link>
-        <Link to="/protected">Protected Page</Link>
+        {/* <Link to="/protected">Protected Page</Link> */}
       </div>
       <div>
         <Route path="/login" component={Login} />
--- auth-friends/src/Components/Login.js
+++ auth-friends/src/Components/Login.js
@@ -1,58 +1,54 @@
 import React from "react"
+import axios from "axios"
 
 class Login extends React.Component {
-    state = {
-        isLoading: {
-          username: '',
-          password: ''
-        }
-      };
-
-      handleChange = error => {
-        this.setState({
-            isLoading: {
-            ...this.state.isLoading,
-            [error.target.name]: error.target.value
-          }
-        });
-      };
-    
-      login = error => {
-        error.preventDefault();
-        axios
-          .post('http://localhost:5000/api/login', this.state.isLoading)
-          .then(response => {
-            localStorage.setItem('token', response.data.payload);
-          })
-          .catch(error => console.log(error.response));
-      };
-    
-      render() {
-        return (
-          <div>
-            <form onSubmit={this.login}>
-              <input
-                type="text"
-                name="username"
-                value={this.state.isLoading.username}
-                onChange={this.handleChange}
-              />
-              <input
-                type="password"
-                name="password"
-                value={this.state.isLoading.password}
-                onChange={this.handleChange}
-              />
-              <button>Log in</button>
-            </form>
-          </div>
-        );
-      }
+  state = {
+    isLoading: {
+      username: "",
+      password: ""
     }
-    
-    export default Login;
-    
-    return <div />
+  }
+
+  handleChange = event => {
+    this.setState({
+      isLoading: {
+        ...this.state.isLoading,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  login = event => {
+    event.preventDefault()
+    axios
+      .post("http://localhost:5000/api/login", this.state.isLoading)
+      .then(response => {
+        localStorage.setItem("token", response.data.payload)
+      })
+      .catch(error => console.log(error.response))
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.isLoading.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.isLoading.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Login
